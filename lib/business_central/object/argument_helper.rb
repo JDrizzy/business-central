591b96c18a583795117452a25f6ea32ece65cc72
Jarrad M <jarrads@live.com>
2020-04-06 16:21:04 +1000

    Add check to confirm instance variables are defined before trying to use them

--- lib/business_central/object/argument_helper.rb
+++ lib/business_central/object/argument_helper.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module BusinessCentral
   module Object
     module ArgumentHelper
       def id(argument)
-        return @id if !@id.nil?
+        return @id if instance_variable_defined?('@id') && !@id.nil?
         return argument.fetch(:id, '') if !argument.nil?
 
         nil
@@ -10,7 +12,7 @@ module BusinessCentral
 
       def company_id(argument)
         return @client.default_company_id if !@client.default_company_id.nil?
-        return @company_id if !@company_id.nil?
+        return @company_id if instance_variable_defined?('@company_id') && !@company_id.nil?
         return argument.fetch(:company_id, '') if !argument.nil?
 
         nil
