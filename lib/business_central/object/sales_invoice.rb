2b1da6e71873c3ac1ff99fbd05987a1f954b593a
Jarrad M <jarrads@live.com>
2020-04-18 20:17:41 +1000

    Add object sales invoice

---
+++ lib/business_central/object/sales_invoice.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module BusinessCentral
+  module Object
+    class SalesInvoice < Base
+      extend BusinessCentral::Object::ObjectHelper
+
+      OBJECT = 'salesInvoices'
+
+      OBJECT_VALIDATION = {
+        customer_purchase_order_reference: {
+          maximum_length: 10
+        },
+        customer_number: {
+          maximum_length: 20
+        },
+        contact_id: {
+          maximum_length: 250
+        },
+        currency_code: {
+          maximum_length: 10
+        },
+        email: {
+          maximum_length: 80
+        },
+        phone: {
+          maximum_length: 30
+        },
+        payment_terms: {
+          maximum_length: 10
+        },
+        shipment_method: {
+          maximum_length: 10
+        },
+        salesperson: {
+          maximum_length: 20
+        },
+        bill_to_name: {
+          maximum_length: 100
+        },
+        bill_to_customer_number: {
+          maximum_length: 20
+        },
+        ship_to_name: {
+          maximum_length: 100
+        },
+        ship_to_contact: {
+          maximum_length: 100
+        }
+      }.freeze
+
+      OBJECT_METHODS = %i[
+        get
+        post
+        patch
+        delete
+      ].freeze
+    end
+  end
+end
