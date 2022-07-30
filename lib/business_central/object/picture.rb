142c5c12fa3d5058fdc282a3f94ec403194b2dd6
Jarrad M <jarrads@live.com>
2022-07-30 13:33:17 +1000

    Remove create method from Picture object since POST doesn't exist

--- lib/business_central/object/picture.rb
+++ lib/business_central/object/picture.rb
@@ -3,18 +3,9 @@
 module BusinessCentral
   module Object
     class Picture < Base
-      def create(data)
-        Request.call(:patch, @client, "#{build_url}/content", etag: '', params: {}) do |request|
-          request['Content-Type'] = 'application/octet-stream'
-          request['If-Match'] = '*'
-          request.body = data
-        end
-      end
-
       def update(id, data)
-        url = build_url(object_id: id)
-        object = Request.get(@client, url)
-        Request.call(:patch, @client, "#{url}/content", etag: object[:etag], params: {}) do |request|
+        object = find_all
+        Request.call(:patch, @client, "#{build_url(object_id: id)}/content", etag: object[:etag], params: {}) do |request|
           request['Content-Type'] = 'application/octet-stream'
           request.body = data
         end
